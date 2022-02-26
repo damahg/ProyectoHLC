9fe0a856ccbb104057a78b80abc6762b5f2f6498
Fran Villa <80397839+FranVilla90@users.noreply.github.com>
2022-02-26 21:09:36 +0100

    Script

--- JS/script.js
+++ JS/script.js
@@ -1,14 +1,15 @@
-var icono = document.getElementById('cambiarModo');
-var fondo = document.getElementById("myBody");
+<script type="text/javascript">
+    
+    var fondo = document.getElementById("myBody");
 
-function cambiarModoOscuro(){
-    //Cambiar a modo oscuro
-    fondo.style.backgroundColor = "black";
-    fondo.style.color = "white";
-}
 
-function cambiarModoClaro(){
-    //Cambiar colores
-    fondo.style.backgroundColor = "white";
-    fondo.style.color = "black";
-}
+    function cambiarColorOscuro (){
+        fondo.style.backgroundColor = "black"
+    }
+
+    function cambiarColorClaro(){
+        fondo.style.backgroundColor = "white"
+    }
+
+
+</script>
