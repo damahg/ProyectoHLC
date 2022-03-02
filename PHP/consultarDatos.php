681b8447d8acba69d8bc4325f73e61831ef1115d
Antonio Hermosín <80785348+damahg@users.noreply.github.com>
2022-03-02 12:29:02 +0100

    Update consultarDatos.php

--- PHP/consultarDatos.php
+++ PHP/consultarDatos.php
@@ -77,41 +77,10 @@
         echo"<table>";
             echo "Numero de alumno -" . "- Nombre -" . "- Apellido1 -". "- Apellido2 -". "- Estudios -" . "- Nota " . "<br>";
             echo "<br>";
-            while ($fila = $alumno->fetch_array()) {
-                echo"<tr>";
-                    echo"<td>";
-                        echo $fila[0];
-                    echo"</td>";
-                    echo"<td>";
-                        echo $fila[2];
-                    echo"</td>";
-                    echo"<td>";
-                        echo $fila[3];
-                    echo"</td>";
-                    echo"<td>";
-                        echo $fila[4];
-                    echo"</td>";
-                    echo"<td>";
-                        echo $fila[5];
-                    echo"</td>";
-                    echo"<td>";
-                        echo $fila[6];
-                    echo"</td>";
-                echo"</tr>";
-
-
-
-            //echo $fila[0] . " - " . $fila[2] . " - " . $fila[3] . " - " . $fila[4] . " - " . $fila[5] . " - " . $fila[6]. "<br>";
+            while ($fila = $alumno->fetch_array()) { 
+                echo $fila[0] . " - " . $fila[2] . " - " . $fila[3] . " - " . $fila[4] . " - " . $fila[5] . " - " . $fila[6]. "<br>";
             }
-
-
-            echo"<tr>";
-                echo"<td>";
-                
-                echo"</td>";
-            echo"</tr>";
-            
-        echo"</table>";
+        
 
         //Buena práctica de programación cerrar la conexión con la mysqli_close()
         mysqli_close($conn);
